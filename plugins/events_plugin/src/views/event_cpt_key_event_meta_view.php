b6d510cbb9e13a7995557d6645e6e47cd239026d
YJohn <yfekadu2016@gmail.com>
2018-03-13 12:11:41 +0000

    created a key event meta

---
+++ plugins/events_plugin/src/views/event_cpt_key_event_meta_view.php
@@ -0,0 +1,29 @@
+<?php
+namespace yohannes\EventsFunctionality\src\views;
+
+	//$custom = get_post_custom($post->ID);
+	// Get the data if its already been entered
+	$_event_cpt_key_event = get_post_meta($post->ID, '_event_cpt_key_event', true);
+
+?>
+<style>
+	._event_cpt_admin_notice select {background-color:#ffb900}
+</style>
+
+	<table>
+
+		<tr>
+		
+			<td>
+			<label><?php _e( 'Key Event?', 'event_cpt' ); ?></label>
+				<input type="radio" name="_event_cpt_key_event" value="0" <?php checked( $_event_cpt_key_event, '0' ); ?> /> Yes<br />
+				<input type="radio" name="_event_cpt_key_event" value="1" <?php checked( $_event_cpt_key_event, '1' ); ?> /> No
+			</td>
+		</tr>
+		<tr id="sponsorLogos">
+			<td>
+				<p>if its yes we will show the image upload link </p>
+			</td>
+		</tr>
+	</table>
+	<?php
