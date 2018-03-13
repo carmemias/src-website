1be0cdf7906b48e4dad5166a9309c587749c6239
YJohn <yfekadu2016@gmail.com>
2018-03-13 11:21:43 +0000

    adjust the size and position for the organizer website

--- plugins/events_plugin/src/views/event_cpt_organizer_meta_view.php
+++ plugins/events_plugin/src/views/event_cpt_organizer_meta_view.php
@@ -20,23 +20,23 @@ namespace yohannes\EventsFunctionality\src\views;
     <tr style="border-bottom: 1px solid #eee;">
 		<td>
 			<label>Main Organizer Name:</label>
-             <input type="text" name="_event_cpt_main_organizer" placeholder="Organizer Name ..." value="<?php echo $_event_cpt_main_organizer; ?>">
+             <input type="text" size="50" name="_event_cpt_main_organizer" placeholder="Organizer Name ..." value="<?php echo $_event_cpt_main_organizer; ?>"><br>
              <label>Other Organizer Name:</label>
-             <input type="text" name="_event_cpt_other_organizer" placeholder="Organizer Name ..." value="<?php echo $_event_cpt_other_organizer; ?>">
+             <input type="text" size="50" name="_event_cpt_other_organizer" placeholder="Organizer Name ..." value="<?php echo $_event_cpt_other_organizer; ?>">
 		</td><!-- organizer name -->
 	</tr>
     <tr>
 		<td>
 			<label>Organizer's Website </label>
-			 <input type="text" size="100" name="_event_cpt_organizer_website" placeholder="Address line 1..." value="<?php echo $_event_cpt_organizer_website; ?>"><br>
+			 <input type="text" size="100" name="_event_cpt_organizer_website" placeholder="website url https:// ....." value="<?php echo $_event_cpt_organizer_website; ?>"><br>
 		</td><!-- venue address -->
 	</tr>
     <tr>
         <td>
-             <label>Social Media </label>
-			 <input type="text" size="100" name="_event_cpt_organizer_facebook" placeholder="Facebook..." value="<?php echo $_event_cpt_organizer_facebook; ?>"><br>
-			 <input type="text" size="100" name="_event_cpt_organizer_twitter" placeholder="Twitter..." value="<?php echo $_event_cpt_organizer_twitter; ?>"><br>
-             <input type="text" size="100" name="_event_cpt_organizer_instagram" placeholder="Instagram..." value="<?php echo $_event_cpt_organizer_instagram; ?>"><br>
+             <label>Social Media </label><br>
+			 <input type="text" size="50" name="_event_cpt_organizer_facebook" placeholder="Facebook..." value="<?php echo $_event_cpt_organizer_facebook; ?>"><br>
+			 <input type="text" size="50" name="_event_cpt_organizer_twitter" placeholder="Twitter..." value="<?php echo $_event_cpt_organizer_twitter; ?>"><br>
+             <input type="text" size="50" name="_event_cpt_organizer_instagram" placeholder="Instagram..." value="<?php echo $_event_cpt_organizer_instagram; ?>"><br>
 		</td><!-- venue address -->
 	</tr>
 
