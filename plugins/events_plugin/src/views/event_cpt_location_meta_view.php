e587b9de0dcc9c31b5f60eab57e9d6056db873b6
YJohn <yfekadu2016@gmail.com>
2018-03-13 10:19:13 +0000

    created an address meta box

--- plugins/events_plugin/src/views/event_cpt_location_meta_view.php
+++ plugins/events_plugin/src/views/event_cpt_location_meta_view.php
@@ -3,6 +3,12 @@ namespace yohannes\EventsFunctionality\src\views;
 
 	//$custom = get_post_custom($post->ID);
 	// Get the data if its already been entered
+	$_event_cpt_venue = get_post_meta($post->ID, '_event_cpt_venue', true);
+	$_event_cpt_address_line_1 = get_post_meta($post->ID, '_event_cpt_address_line_1', true);
+	$_event_cpt_address_line_2 = get_post_meta($post->ID, '_event_cpt_address_line_2', true);
+	$_event_cpt_address_town_city = get_post_meta($post->ID, '_event_cpt_address_town_city', true);
+	$_event_cpt_address_county = get_post_meta($post->ID, '_event_cpt_address_county', true);
+	$_event_cpt_address_postcode = get_post_meta($post->ID, '_event_cpt_address_postcode', true);
 	$_event_cpt_area = get_post_meta($post->ID, '_event_cpt_area', true);
 
 ?>
@@ -11,6 +17,29 @@ namespace yohannes\EventsFunctionality\src\views;
 </style>
 
 <table class="form-table"> <!-- TODO make translatable -->
+	<tr style="border-bottom: 1px solid #eee;">
+		<td>
+			<label>Venue Name:</label>
+			 <input type="text" name="_event_cpt_venue" placeholder="Venue name ..." value="<?php echo $_event_cpt_venue; ?>">
+		</td><!-- venue name -->
+	</tr>
+	<tr>
+		<td>Type the Event's Address:</td>
+	</tr>
+	 <tr>
+		<td>
+			<label>Address Line 1: </label>
+			 <input type="text" name="_event_cpt_address_line_1" placeholder="Address line 1..." value="<?php echo $_event_cpt_address_line_1; ?>">
+			 <label>Address Line 2: </label>
+			 <input type="text" name="_event_cpt_address_line_2" placeholder="Address line 2..." value="<?php echo $_event_cpt_address_line_2; ?>">
+			 <label>Town/city </label>
+			 <input type="text" name="_event_cpt_address_town_city" placeholder="Town / City..." value="<?php echo $_event_cpt_address_town_city; ?>">
+			 <label>County: </label>
+			 <input type="text" name="_event_cpt_address_county" placeholder="County..." value="<?php echo $_event_cpt_address_county ?>">
+			 <label>postCode: </label>
+			 <input type="text" name="_event_cpt_address_postcode" placeholder="postcode..." value="<?php echo $_event_cpt_address_postcode; ?>">	 
+		</td><!-- venue address -->
+	</tr>
 	<tr>
 		<td>Type the Event's area name:</td>
 	</tr>
