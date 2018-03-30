4554c55783a41f018b4bbe7c735a8882dbdb0817
Etza <topimorita@gmail.com>
2018-03-30 15:57:52 +0100

    Styled search bar and newsletter form

--- themes/src-project/functions.php
+++ themes/src-project/functions.php
@@ -144,7 +144,7 @@ function src_project_scripts() {
 
 	wp_enqueue_script( 'src-project-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'src-project-search', get_template_directory_uri() . '/js/searchButton.js', array('jquery'), null, true );
+	wp_enqueue_script( 'src-project-search', get_template_directory_uri() . '/js/searchBttn.js', array('jquery'), null, true );
 
 	wp_enqueue_script( 'src-project-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
