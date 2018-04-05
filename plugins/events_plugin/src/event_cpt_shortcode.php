c1345a42fa258e7e831ba3ac3ebbf839539b7d33
carmemias <info@carmemias.com>
2018-04-05 18:24:04 +0100

    refactor

--- plugins/events_plugin/src/event_cpt_shortcode.php
+++ plugins/events_plugin/src/event_cpt_shortcode.php
@@ -120,78 +120,38 @@ function events_cpt_shortcode_handler( $atts ){
     //get the data
     $event_id = $single_event->ID;
     $event_name = get_the_title($event_id);
-    $event_types = get_the_terms( $event_id, 'event-type' );
+    //test $event_types = get_the_terms( $event_id, 'event-type' );
+    $event_types = get_event_types($event_id);
     $event_types_string = '';
-    $event_image = get_the_post_thumbnail( $event_id, 'thumbnail' );
-    $event_description = '';
-    //$event_description = apply_filters( 'the_content', get_the_content() );
-
+    $event_image = get_the_post_thumbnail( $event_id, 'medium' );
     $event_excerpt = get_the_excerpt( $event_id );
     $event_date = date('l, j F', strtotime($single_event->_event_cpt_date_event));
     $event_start_time = $single_event->_event_cpt_startTime_event;
     $event_end_time = $single_event->_event_cpt_endTime_event;
-    $event_organiser_main = $single_event->_event_cpt_main_organizer;
-    $event_organiser_other_1 = $single_event->_event_cpt_other_organizer_1;
-    $event_organiser_other_2 = $single_event->_event_cpt_other_organizer_2;
-    $event_organiser_other_3 = $single_event->_event_cpt_other_organizer_3;
-    $event_organiser_website = $single_event->_event_cpt_organizer_website;
-    $event_organiser_facebook = $single_event->_event_cpt_organizer_facebook;
-    $event_organiser_twitter = $single_event->_event_cpt_organizer_twitter;
-    $event_organiser_instagram = $single_event->_event_cpt_organizer_instagram;
-    $event_venue = $single_event->_event_cpt_venue;
-    $event_address_line_1 = $single_event->_event_cpt_address_line_1;
-    $event_address_line_2 = $single_event->_event_cpt_address_line_2;
-    $event_postcode = $single_event->_event_cpt_address_postcode;
-    $event_area = $single_event->_event_cpt_area;
+    $event_organisers = get_event_organisers($single_event);
+    $event_organiser_links = get_event_organiser_links($single_event);
+
+    $event_location = get_event_full_location($single_event);
+
     $event_price = money_format('%i', floatval($single_event->_event_cpt_price_event));
     $event_post_url = get_permalink($event_id);
 
-    //if more than 1 event type, join them.
-    if ( $event_types && !is_wp_error( $event_types ) ) {
-        $event_types_array = array();
-        foreach ( $event_types as $event_type ) {
-            $event_types_array[] = $event_type->name;
-        }
-        $event_types_string = join( " | ", $event_types_array );
-        }
-
     //output the event
-    $output_string .= '<section id="event-'.$event_id.'" class="event-entry">';
+    $output_string .= '<section id="event-'.esc_attr($event_id).'" class="event-entry">';
     $output_string .= '<div class="left-column">';
     $output_string .= '<a href="'.esc_url_raw($event_post_url).'" alt="Read more about '.$event_name.'">'.$event_image.'</a>';
-    $output_string .= '<div class="links">';
-    if( '' != $event_organiser_website ){$output_string .= '<a href="'.esc_url_raw($event_organiser_website).'" target="_blank" rel="noopener"><span class="screen-reader-text">Website</span><svg class="icon icon-website" aria-hidden="true" role="img"><use href="#icon-website" xlink:href="#icon-website"></use></svg></a>';}
-    if( '' != $event_organiser_facebook ){$output_string .= '<a href="'.esc_url_raw($event_organiser_facebook).'" target="_blank" rel="noopener"><span class="screen-reader-text">Facebook</span><svg class="icon icon-facebook" aria-hidden="true" role="img"><use href="#icon-facebook" xlink:href="#icon-facebook"></use></svg></a>';}
-    if( '' != $event_organiser_twitter ){$output_string .= '<a href="'.esc_url_raw($event_organiser_twitter).'" target="_blank" rel="noopener"><span class="screen-reader-text">Twitter</span><svg class="icon icon-twitter" aria-hidden="true" role="img"><use href="#icon-twitter" xlink:href="#icon-twitter"></use></svg></a>';}
-    if( '' != $event_organiser_instagram ){$output_string .= '<a href="'.esc_url_raw($event_organiser_instagram).'" target="_blank" rel="noopener"><span class="screen-reader-text">Instagram</span><svg class="icon icon-instagram" aria-hidden="true" role="img"><use href="#icon-instagram" xlink:href="#icon-instagram"></use></svg></a>';}
-    $output_string .= '</div><!-- links -->';
+    $output_string .= $event_organiser_links;
     $output_string .= '</div><!-- left-column -->';
+
     $output_string .= '<div class="right-column">';
     $output_string .= ' <h2 class="type-title"><a href="'.esc_url_raw($event_post_url).'" alt="Read more about '.$event_name.'">' . $event_name . '</a></h2>';
-    $output_string .= ' <div class="entry-meta">'.$event_types_string.'</div>';
+    $output_string .= ' <div class="entry-meta">'.$event_types.'</div>';
     $output_string .= '<div class="event-excerpt">';
     if(''!=$event_excerpt){$output_string .= $event_excerpt;} else {$output_string .= 'No excerpt yet.';}
     $output_string .= '</div>';//$output_string .= $event_description;
-    $output_string .= '<p class="organisers">Organised by: '.$event_organiser_main.'.';
-    if( $event_organiser_other_1 || $event_organiser_other_2 || $event_organiser_other_3 ){
-      $other_organisers = array();
-      if($event_organiser_other_1){array_push($other_organisers,$event_organiser_other_1);}
-      if($event_organiser_other_2){array_push($other_organisers,$event_organiser_other_2);}
-      if($event_organiser_other_3){array_push($other_organisers,$event_organiser_other_3);}
-      $output_string .= ' In partnership with: '. $other_organisers[0];
-      if(2 === count($other_organisers)){
-        $output_string .= ' and '.$event_organiser_other_2.'.';
-      }
-      if(3 === count($other_organisers)){
-        $output_string .= ', '.$event_organiser_other_2.' and '.$event_organiser_other_3.'.';
-      }
-    }
-    $output_string .= '</p><!-- organisers -->';
+    $output_string .= $event_organisers;
     $output_string .= ' <p class="date">'.$event_date.' from '.$event_start_time.' to '.$event_end_time.'</p>';
-    $output_string .= ' <p class="location">'.$event_venue;
-    if($event_address_line_1){$output_string .= ', '.$event_address_line_1;}
-    if($event_address_line_2){$output_string .= ', '.$event_address_line_2;}
-    $output_string .= ', '.$event_area.' '.$event_postcode.'</p>';
+    $output_string .= $event_location;
     $output_string .= '<p class="price">';
     if('0.00' != $event_price){$output_string .= '£'.$event_price;}else{$output_string .= 'FREE';};
     $output_string .= '</p>';
@@ -221,8 +181,86 @@ add_filter( 'excerpt_more', __NAMESPACE__.'\custom_excerpt_more' );
 * Limit the excerpt to 20 words
 */
 function custom_excerpt_length( $length ) {
-	return 20;
+	return 18;
 }
 add_filter( 'excerpt_length',  __NAMESPACE__.'\custom_excerpt_length', 999 );
+/*
+* Get string listing event types
+*/
+function get_event_types($id){
+ $types = get_the_terms( $id, 'event-type' );
+ $types_string = '';
 
+ if ( $types && !is_wp_error( $types ) ) {
+    $types_array = array();
+    foreach ( $types as $type ) { $types_array[] = $type->name;}
+    $types_string = join( " | ", $types_array );
+  }
+
+  return $types_string;
+}
+
+/*
+* Get the organisers blurb
+*/
+function get_event_organisers($event){
+  $string ='';
+  $event_organiser_main = $event->_event_cpt_main_organizer;
+  $event_organiser_other_1 = $event->_event_cpt_other_organizer_1;
+  $event_organiser_other_2 = $event->_event_cpt_other_organizer_2;
+  $event_organiser_other_3 = $event->_event_cpt_other_organizer_3;
+
+  $string .= '<p class="organisers">Organised by: '.$event_organiser_main.'.';
+  if( $event_organiser_other_1 || $event_organiser_other_2 || $event_organiser_other_3 ){
+    $other_organisers = array();
+    if($event_organiser_other_1){array_push($other_organisers,$event_organiser_other_1);}
+    if($event_organiser_other_2){array_push($other_organisers,$event_organiser_other_2);}
+    if($event_organiser_other_3){array_push($other_organisers,$event_organiser_other_3);}
+    $string .= ' In partnership with: '. $other_organisers[0];
+    if(2 === count($other_organisers)){
+      $string .= ' and '.$event_organiser_other_2.'.';
+    }
+    if(3 === count($other_organisers)){
+      $string .= ', '.$event_organiser_other_2.' and '.$event_organiser_other_3.'.';
+    }
+  }
+  $string .= '</p><!-- organisers -->';
+
+  return $string;
+}
+
+function get_event_organiser_links($event){
+  $string ='';
+
+  $event_organiser_website = $event->_event_cpt_organizer_website;
+  $event_organiser_facebook = $event->_event_cpt_organizer_facebook;
+  $event_organiser_twitter = $event->_event_cpt_organizer_twitter;
+  $event_organiser_instagram = $event->_event_cpt_organizer_instagram;
+
+  $string .= '<div class="links">';
+  if( '' != $event_organiser_website ){$string .= '<a href="'.esc_url_raw($event_organiser_website).'" target="_blank" rel="noopener"><span class="screen-reader-text">Website</span><svg class="icon icon-website" aria-hidden="true" role="img"><use href="#icon-website" xlink:href="#icon-website"></use></svg></a>';}
+  if( '' != $event_organiser_facebook ){$string .= '<a href="'.esc_url_raw($event_organiser_facebook).'" target="_blank" rel="noopener"><span class="screen-reader-text">Facebook</span><svg class="icon icon-facebook" aria-hidden="true" role="img"><use href="#icon-facebook" xlink:href="#icon-facebook"></use></svg></a>';}
+  if( '' != $event_organiser_twitter ){$string .= '<a href="'.esc_url_raw($event_organiser_twitter).'" target="_blank" rel="noopener"><span class="screen-reader-text">Twitter</span><svg class="icon icon-twitter" aria-hidden="true" role="img"><use href="#icon-twitter" xlink:href="#icon-twitter"></use></svg></a>';}
+  if( '' != $event_organiser_instagram ){$string .= '<a href="'.esc_url_raw($event_organiser_instagram).'" target="_blank" rel="noopener"><span class="screen-reader-text">Instagram</span><svg class="icon icon-instagram" aria-hidden="true" role="img"><use href="#icon-instagram" xlink:href="#icon-instagram"></use></svg></a>';}
+  $string .= '</div><!-- links -->';
+
+  return $string;
+}
+
+function get_event_full_location($event){
+  $string ='';
+
+  $event_venue = $event->_event_cpt_venue;
+  $event_address_line_1 = $event->_event_cpt_address_line_1;
+  $event_address_line_2 = $event->_event_cpt_address_line_2;
+  $event_postcode = $event->_event_cpt_address_postcode;
+  $event_area = $event->_event_cpt_area;
+
+  $string .= ' <p class="location">'.$event_venue;
+  if($event_address_line_1){$string .= ', '.$event_address_line_1;}
+  if($event_address_line_2){$string .= ', '.$event_address_line_2;}
+  $string .= ', '.$event_area.' '.$event_postcode.'</p>';
+
+  return $string;
+}
 ?>
