09ba168b311cc0333a1c9109e4701b2ef37c0366
carmemias <info@carmemias.com>
2018-05-06 15:13:51 +0100

    fixed bug re search icon url

--- themes/src-project/js/searchBttn.js
+++ themes/src-project/js/searchBttn.js
@@ -23,58 +23,21 @@
 
     lastItem = links[links.length - 1];
 
-/* OPTION 1 - DOES NOT WORK ON IPHONE
-<a href="#" class="search-bttn">
- <span class="screen-reader-text">Search</span>
- <svg class="icon icon-search" role="img" aria-hidden="true">
-  <use href="#icon-search" xlink:href="#icon-search"></use>
- </svg>
-</a>
-*/
-/* OPTION 2 - TESTING
-<object type=”image/svg+xml” data=”mySVG.svg”>
-  <div id="fallback"></div>
-</object>
-*/
     if (lastItem.innerHTML === 'Search' || 'search') {
-  /*OPTION 1    let spanEl = document.createElement('span'),
-        svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-        useEl = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-
-      spanEl.innerHTML = 'Search';
-      spanEl.classList.add('screen-reader-text');
-
-      useEl.setAttribute('href', '#icon-search');
-      useEl.setAttribute('xlink:href', '#icon-search');
-
-      svgEl.setAttribute('class', 'icon icon-search');
-      svgEl.setAttribute('role', 'img');
-      svgEl.setAttribute('aria-hidden', 'true');
-      svgEl.appendChild(useEl);
-      svgEl.appendChild(imgFallback);
-
-      lastItem.innerHTML = '';
-      lastItem.appendChild(spanEl);
-
-      lastItem.appendChild(svgEl);
-
-      lastItem.classList.add('search-bttn');
-      lastItem.addEventListener('click', toggleSearchForm); */
-
-      /* OPTION 2 */
       let spanEl = document.createElement('span'),
-          //objEl = document.createElementNS('http://www.w3.org/2000/svg', 'object');
+          protocol = window.location.protocol,
+          hostURL = window.location.hostname,
+          svgURL = protocol + '//' + hostURL + '/wp-content/themes/src-project/images/search-icon.svg',
+          pngURL = protocol + '//' + hostURL + '/wp-content/themes/src-project/images/search-icon.png',
           imgEl = document.createElement('img'); //<img src="your.svg" onerror="this.src='your.png'">
 
       spanEl.innerHTML = 'Search';
       spanEl.classList.add('screen-reader-text');
 
-      imgEl.setAttribute('src', 'http://localhost/wp-content/themes/src-project/images/search-icon.svg');
-      imgEl.setAttribute('onerror', "this.src='http://localhost/wp-content/themes/src-project/images/search-icon.png'");
+      imgEl.setAttribute('src', svgURL);
+      imgEl.setAttribute('onerror', "this.src=pngURL");
       imgEl.setAttribute('width', 25);
       imgEl.setAttribute('height', 25);
-      //objEl.setAttribute( 'type', 'image/svg+xml'); //type=”image/svg+xml”
-      //objEl.setAttribute( 'data', 'http://localhost/wp-content/themes/src-project/images/search-icon.svg');
 
       lastItem.innerHTML = '';
       lastItem.appendChild(spanEl);
