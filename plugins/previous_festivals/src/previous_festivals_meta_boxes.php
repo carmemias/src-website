abf9b8571ef487e7c664ffc57e902eab08f82eb2
Etza <topimorita@gmail.com>
2018-05-19 16:31:20 +0100

    Working on saving data from previous festivals plugin

--- plugins/previous_festivals/src/previous_festivals_meta_boxes.php
+++ plugins/previous_festivals/src/previous_festivals_meta_boxes.php
@@ -20,12 +20,13 @@ function previous_festivals_enqueue_logo_script() {
     if ( ! did_action( 'wp_enqueue_media' ) ) {
         wp_enqueue_media();
     }
-
-     //wp_enqueue_script( 'eventlogoscript' , EVENT_FUNCTIONALITY_URL .'/src/assets/js/upload_event_logo.js', array('jquery'), null, true );
-     //wp_enqueue_style( 'eventmetastyles' , EVENT_FUNCTIONALITY_URL .'/src/assets/css/metaboxStyling.css');
+        wp_enqueue_style( 'wp-color-picker');
+        wp_enqueue_script( 'wp-color-picker');
+        wp_enqueue_script( 'previousfestivalsscript' , PREVIOUS_FESTIVAL_PLUGIN_URL .'/src/assets/previousfestivalsscript.js', array('jquery'), null, true );
+       // wp_enqueue_style( 'previousfestivalsstyles' , PREVIOUS_FESTIVAL_PLUGIN_URL .'/src/assets/previousfestivalsstyles.css');
 }
 
-//add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\previous_festivals_enqueue_logo_script' );
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\previous_festivals_enqueue_logo_script' );
 
 function previous_festivals_add_meta_box(){	
 	add_meta_box( 'previous_festivals_meta', 'Previous Festivals', __NAMESPACE__.'\render_previous_festivals_meta', 'page', 'side', 'low', array() );
@@ -40,3 +41,44 @@ function render_previous_festivals_meta(){
 
 	require_once plugin_dir_path(__FILE__).'previous_festivals_meta_view.php';
 }
+
+function save_previous_festivals_meta($post_id, $post){
+	if ( ! isset( 
+	$_POST['previous_festivals_noncename']
+	) ) { return; }
+	// verify this came from the our screen and with proper authorization, because save_post can be triggered at other times
+	if( ( 
+	plugin_basename(__FILE__) ) || !wp_verify_nonce( $_POST['previous_festivals_noncename']
+	 )) {
+						return $post->ID;}
+
+	// is the user allowed to edit the post or page?
+	if( ! current_user_can( 'edit_post', $post->ID )){
+						return $post->ID;}
+
+	// ok, we're authenticated: we need to find and save the data. We'll put it into an array to make it easier to loop through
+	$previous_festival_meta['_previous_festival_is_programme'] = absint($_POST['_previous_festival_is_programme']);
+	$previous_festival_meta['_previous_festival_is_current'] = absint($_POST['_previous_festival_is_current']);
+	$previous_festival_meta['_previous_festival_start_date'] = $_POST['_previous_festival_start_date'];
+	$previous_festival_meta['_previous_festival_end_date'] = $_POST['_previous_festival_end_date'];
+    $previous_festival_meta['_previous_festival_social_media_buttons_colour'] = sanitize_hex_color($_POST['_previous_festival_social_media_buttons_colour']);
+    $previous_festival_meta['_previous_festival_text_colour'] = sanitize_hex_color($_POST['_previous_festival_text_colour']);
+    $previous_festival_meta['_previous_festival_accent_colour'] = sanitize_hex_color($_POST['_previous_festival_accent_colour']);
+
+	// Add values of $events_meta as custom fields
+	foreach ($previous_festival_meta as $key => $value) { // Cycle through the $classes_meta array!
+		if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+
+		$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+
+		if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+		     update_post_meta($post->ID, $key, $value);
+		} else { // If the custom field doesn't have a value
+		     add_post_meta($post->ID, $key, $value);
+		}
+
+		if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+	}
+}
+
+add_action('save_post', __NAMESPACE__.'\save_previous_festivals_meta', 1, 2);
--- plugins/previous_festivals/src/previous_festivals_meta_view.php
+++ plugins/previous_festivals/src/previous_festivals_meta_view.php
@@ -4,6 +4,14 @@ namespace etzali\PreviousFestivals\src;
 global $post;
 $is_programme = get_post_meta($post->ID, '_previous_festival_is_programme', true);
 $is_current = get_post_meta($post->ID, '_previous_festival_is_current', true);
+$start_date = get_post_meta($post->ID, '_previous_festival_start_date', true);
+$end_date = get_post_meta($post->ID, '_previous_festival_end_date', true);
+$social_media_colour = get_post_meta($post->ID, '_previous_festival_social_media_buttons_colour', true);
+if(empty($social_media_colour)){$social_media_colour = "#a66bed";}
+$text_colour = get_post_meta($post->ID, '_previous_festival_text_colour', true);
+if(empty($text_colour)){$text_colour = "#2f39ed";}
+$accent_colour = get_post_meta($post->ID, '_previous_festival_accent_colour', true);
+if(empty($accent_colour)){$accent_colour = "#0ea2c7";}
 
 ?>
 	<style>
@@ -24,6 +32,21 @@ $is_current = get_post_meta($post->ID, '_previous_festival_is_current', true);
 			<label><?php _e( 'Is this for the current year?', 'previous-festivals' ); ?></label><br>
 				<input type="radio" name="_previous_festival_is_current" value="1" <?php checked( $is_current, '1' ); ?> /> Yes<br />
 				<input type="radio" name="_previous_festival_is_current" value="0" <?php checked( $is_current, '0' ); ?> /> No
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label><?php _e( 'What are the festival dates?', 'previous-festivals' ); ?></label><br>
+				<input type="date" name="_previous_festival_start_date" value="1" <?php checked( $start_date ); ?> /> <br />
+				<input type="date" name="_previous_festival_end_date" value="0" <?php checked( $end_date ); ?> /> 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<label><?php _e( 'Festival Colour Scheme', 'previous-festivals' ); ?></label><br />
+				<label>Social Media Buttons </label> <input class="color-field" type="text" name="_previous_festival_social_media_buttons_colour" value="<?php esc_attr_e($social_media_colour); ?>" /> <br />
+				<label>Text Colour </label><input  class="color-field" type="text" name="_previous_festival_text_colour" value=" <?php esc_attr_e($text_colour); ?>" /> <br />
+				<label>Accent Colour </label><input  class="color-field" type="text" name="_previous_festival_accent_colour" value="<?php esc_attr_e( $accent_colour); ?>" /> <br />
 		</td>
 	</tr>
 </table>
